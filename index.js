b5ae9e3955a48b8b8b78d7dbf707c9689cb00389
Dustin Gogoll <dustin.gogoll@googlemail.com>
2018-09-18 21:35:10 +0200

    Backend configuration

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const app = express()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const db = require('./db');
-
-app.use(express.static('public'));
-app.set('view engine', 'ejs')
-
-app.get('/', function (req, res) {
-  res.render('index');
-})
 
 app.use(function(request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
@@ -18,9 +11,9 @@ app.use(function(request, response, next) {
 
 //Get random contract
 app.get('/getData', function(request, response, next){
-  db.query(`SELECT * FROM ${request.query.table} ORDER BY RANDOM() limit 1`, function(error, results){
+  db.query(`SELECT * FROM Employee_lastName ORDER BY RANDOM() limit 1`, function(error, results){
     if (error){
-      response.status(400).send('Error in database operation');
+      response.status(400).send(error);
     } else {
       response.send(results);
     }
