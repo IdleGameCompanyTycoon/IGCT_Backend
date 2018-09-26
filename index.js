9a96862ef70c4c080899f271555182d1634c769a
Dustin Gogoll <dustin.gogoll@googlemail.com>
2018-09-26 18:25:40 +0200

    added basic dboperarions api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 5000;
-const db = require('./db');
+const dbOperations = require('./apis/dbOperations');
 
 app.use(function(request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
@@ -11,13 +11,7 @@ app.use(function(request, response, next) {
 
 //Get random contract
 app.get('/getData', function(request, response, next){
-  db.query(`SELECT * FROM Employee_lastName ORDER BY RANDOM() limit 1`, function(error, results){
-    if (error){
-      response.status(400).send(error);
-    } else {
-      response.send(results);
-    }
-  })
+  dbOperations['getContract']();
 })
 
 
