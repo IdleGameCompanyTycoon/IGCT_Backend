b1da363451256cff0538dc061768e458c2c0e80f
Nalun <raphaelgrimm@icloud.com>
2019-09-12 22:07:09 +0200

    added project manager to templates

--- apis/employeeTemplates.js
+++ apis/employeeTemplates.js
@@ -12,7 +12,6 @@ const gameDevSkills = {
   tycoon: 0.5,
 }
 
-// Templates
 const softwareDevSkills = {
   network: 0.5,
   uiDev: 0.5,
@@ -20,22 +19,41 @@ const softwareDevSkills = {
   osDev: 0.5,
 }
 
+const teamBuffSkills = {
+  mgmt: 0.3,
+  speed: 0.1,
+}
+
+// Templates
 const developer = {
   speed: 0.5,
   softwareDev: {multiplier: 0.65},
   gameDev: {multiplier: 0.65},
+  social: 0.6,
+  gameSkills: gameDevSkills,
+  softwareSkills: softwareDevSkills,
+}
+
+const projectManager = {
+  speed: 0.3,
+  softwareDev: {multiplier: 0.3},
+  gameDev: {multiplier: 0.3},
+  teamBuff: {multiplier: 0.65},
   social: 0.8,
   gameSkills: gameDevSkills,
-  softwareSkills: softwareDevSkills
+  softwareSkills: softwareDevSkills,
+  teamSkills: teamBuffSkills,
 }
 
 // Maps the multiplicators on the special skills
 const skillSetMapping = {
   gameSkills: 'gameDev',
   softwareSkills: 'softwareDev',
+  teamSkills: 'teamBuff',
 }
 
 module.exports = {
+  projectManager: projectManager,
   developer: developer,
-  skillSetMapping: skillSetMapping
+  skillSetMapping: skillSetMapping,
 }
