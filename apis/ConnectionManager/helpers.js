a9cbb2331fbb1333effac54f422908c559b288ac
Nalun <raphaelgrimm@icloud.com>
2019-11-28 18:26:13 +0100

    fixed array declaration

--- apis/ConnectionManager/helpers.js
+++ apis/ConnectionManager/helpers.js
@@ -6,25 +6,11 @@ const { LOG_LEVEL } = require('../../settings.json');
 const writeLog = (severity = "3", message) => {
   if(severity <= LOG_LEVEL){
 
-    let month = new Array();
-    month[0] = "1";
-    month[1] = "2";
-    month[2] = "3";
-    month[3] = "4";
-    month[4] = "5";
-    month[5] = "6";
-    month[6] = "7";
-    month[7] = "8";
-    month[8] = "9";
-    month[9] = "10";
-    month[10] = "11";
-    month[11] = "12";
+    let month = [];
+    month = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
 
-    let severityTranslation = new Array();
-    severityTranslation[0] = "IP";
-    severityTranslation[1] = "ERROR";
-    severityTranslation[2] = "WARNING";
-    severityTranslation[3] = "INFO";
+    let severityTranslation = [];
+    severityTranslation = ["IP", "ERROR", "WARNING", "INFO"];
 
     let time = getDateTime();
     let line = time + " - [" + severityTranslation[severity] + "] - " + message + "\n";
