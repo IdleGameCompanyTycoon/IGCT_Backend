3fda42590a3e48ea3b234971dec9cdb98bb02d02
Nalun <raphaelgrimm@icloud.com>
2019-12-09 21:50:34 +0100

    mongoose bullshit

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const path = require('path')
 const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const ConnectionManager = require('./apis/ConnectionManager/ConnectionManager');
 const { writeLog } = require('./apis/ConnectionManager/helpers');
-
+const User = require('./apis/Authentication/user');
 const dir = path.join(__dirname, 'public/img');
 
 app.use('/images', express.static(dir));
+
+app.use(bodyParser.json());
+
+
 
 app.use((request, response, next) => {
   response.header("Access-Control-Allow-Origin", "*");
@@ -17,6 +23,18 @@ app.use((request, response, next) => {
 
 const connectionManager = new ConnectionManager();
 writeLog("3", "Connection esablished");
+console.log(User);
+app.post('/auth', (request, response) => {
+  const user = new User({
+    username: request.body.username,
+    password: request.body.password
+  }).save((err, response) => {
+    if(err){
+      res.status(400).send(err);
+      res.status(200).send(response);
+    }
+  })
+});
 
 //Get data
 app.get('/getData', (request, response, next) => {
