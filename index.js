e432de61b6e0e04ff6966cfd4df6ec327c42bf5b
Nalun <raphaelgrimm@icloud.com>
2019-12-15 17:53:21 +0100

    https test

--- index.js
+++ index.js
@@ -128,5 +128,5 @@ app.get('/getData', (request, response, next) => {
                 });
 })
 
-//https.createServer(options, app).listen(PORT);
-app.listen(PORT);
+https.createServer(options, app).listen(PORT);
+//app.listen(PORT);
